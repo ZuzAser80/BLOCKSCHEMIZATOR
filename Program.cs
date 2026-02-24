e0085bdbf13756337adee3e9c2c75a37b18bc028
BastrakovDanil <dde4th@gmail.com>
2026-02-24 13:31:17 +0500

    for fixed (again)

--- Program.cs
+++ Program.cs
@@ -101,7 +101,7 @@ public class DrawIoGenerator
             max_y = y;
         }
 
-        if ((_types.Any(s.Contains) && s.Contains("{") && s.Contains("(") && s.Contains(")")) || s.Contains("return") && !s.Contains("for"))
+        if ((_types.Any(s.Contains) && s.Contains("{") && s.Contains("(") && s.Contains(")") && !s.Contains("for")) || s.Contains("return"))
         {
             return RoundBox(id, s.Replace('{', ' ').TrimEnd(), x, y);
         }
